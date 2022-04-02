d0d7ffc8afabe6a283f6393a00294944ee3fdd86
Maddox Twin1 <jaspertena@aol.com>
2022-04-01 22:41:41 -0400

    correct timeblock events

--- Develop/script.js
+++ Develop/script.js
@@ -1,43 +1,71 @@
-// Add a current day to the top of the calendar
-var current = moment().format('dddd MMMM DD, YYYY');
+// Add a current day to the top of the calendar using moment API
+var current = moment().format('dddd MMMM DD, YYYY LT');
 
 function setDate () {
     var now = document.getElementById("currentDay");
     now.textContent=current;
 }
 setDate();
-// Create timeblock for the current day standard business hours (8 am to 5 pm  )
-// Color code the blocks based on past, present and future
-// Enable adding events to the calendar
-// Add an event button to to save any changes added to the calendar
-// When refreshed all changes are saved to
-console.log(current)
+
+// create conditional function to access the timeblock event color
 $(".description").each(function(){
     var rightnow = moment().hour();
-   
+//var rightnow to define current time vs future&present
     var timeblock =parseInt($(this).parent().attr("id"));
-    console.log(timeblock);
+// parseInt used to covert id#9 to interger to compare to current time
     if(timeblock===rightnow){
         $(this).addClass("present");
-        // console.log("present")
+// if timeblock and right now are equal then link the present class styling
     }
     else if(timeblock>rightnow){
         $(this).addClass("future");
-        // console.log("future")
+ // if timeblock is greater than right now link the future class styling    
     }
     else {
         $(this).addClass("past")
-        // console.log("past")
+// for any other option use past styling 
     }
 })
 $(".saveBtn").on("click", function(){
     var task = $(this).siblings(".description").val();
-    console.log(task);
+    // console.log(task);
     localStorage.setItem("task",task);
 }) 
-
+// var task used to save the events entered in the local storage
 $(".description").each(function(){
     var timeblock = $(this).parent().attr("id");
     $("#9 .description").val(localStorage.getItem("task"));
-
+// saves the event for each hour only in the specified timeblock
+})
+$(".description").each(function(){
+    var timeblock = $(this).parent().attr("id");
+    $("#10 .description").val(localStorage.getItem("task"));
+})
+$(".description").each(function(){
+    var timeblock = $(this).parent().attr("id");
+    $("#11 .description").val(localStorage.getItem("task"));
+})
+$(".description").each(function(){
+    var timeblock = $(this).parent().attr("id");
+    $("#12 .description").val(localStorage.getItem("task"));
+})
+$(".description").each(function(){
+    var timeblock = $(this).parent().attr("id");
+    $("#13 .description").val(localStorage.getItem("task"));
+})
+$(".description").each(function(){
+    var timeblock = $(this).parent().attr("id");
+    $("#14 .description").val(localStorage.getItem("task"));
+})
+$(".description").each(function(){
+    var timeblock = $(this).parent().attr("id");
+    $("#15 .description").val(localStorage.getItem("task"));
+})
+$(".description").each(function(){
+    var timeblock = $(this).parent().attr("id");
+    $("#16 .description").val(localStorage.getItem("task"));
+})
+$(".description").each(function(){
+    var timeblock = $(this).parent().attr("id");
+    $("#17 .description").val(localStorage.getItem("task"));
 })
